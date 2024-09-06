78912d9c75cfc3f86e8374084a9b2afbe7a98ffd
Diego Benitez <criptograma12@gmail.com>
2024-09-05 23:53:09 -0300

    test github

--- WindowsFormsApp/Program.cs
+++ WindowsFormsApp/Program.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsApp
         [STAThread]
         static void Main()
         {
+            //test github
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
